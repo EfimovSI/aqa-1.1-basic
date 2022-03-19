5cf5f6c1b5d35157666c15f99e3b243a8cfab08d
Sergey Efimov <yefimovsergey00@gmail.com>
2022-03-19 23:39:15 +0300

    "TestNG used"

---
+++ src/test/java/ru/netology/unit/service/CashbackHackServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.unit.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturn150WhenAmountIs850() {
+        assertEquals(service.remain(850), 150);
+    }
+
+    @Test
+    public void shouldReturn999WhenAmountIs2001() {
+        assertEquals(service.remain(2001), 999);
+    }
+
+    @Test
+    public void shouldReturn1000WhenAmountIs3000() {
+        assertEquals(service.remain(3000), 1000);
+    }
+
+    @Test
+    public void shouldReturn1000WhenAmountIs0() {
+        assertEquals(service.remain(0), 1000);
+    }
+}
