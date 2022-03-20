a831c93e560b85a2110ceffc026364eeb9df227a
Sergey Efimov <yefimovsergey00@gmail.com>
2022-03-20 17:22:56 +0300

    "Task2 completed"

--- src/test/java/ru/netology/unit/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.unit.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
